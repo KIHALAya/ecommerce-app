01bf9fc32230ab4ae1b4025dc053ee960bc40ef0
KIHALAya <kihalaya00@gmail.com>
2025-01-02 23:18:02 +0100

    Adding Listing Products Functionality

--- admin_panel/src/App.jsx
+++ admin_panel/src/App.jsx
@@ -10,7 +10,8 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
-export const backendURL = import.meta.env.VITE_BACKEND_URL
+export const backendURL = import.meta.env.VITE_BACKEND_URL;
+export const currency = 'MAD';
 
 const App = () => {
 
